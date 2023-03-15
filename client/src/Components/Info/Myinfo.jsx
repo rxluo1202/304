7a41269dc70e07a64499104318fb4fd32a5bc56e
Mengya Zeng <echomaria98123@gmail.com>
2023-03-15 13:04:19 -0700

    itv page done

--- client/src/Components/Info/Myinfo.jsx
+++ client/src/Components/Info/Myinfo.jsx
@@ -1,112 +1,135 @@
-import React, { useState, useContext } from 'react';
-import Interviews from './Interviews';
-import Jobs from './Jobs';
-import Applications from './Applications';
-import { BrowserRouter as Router,
-    Routes,
-    Route,
-    NavLink,
-    useParams,
-    useMatch } from 'react-router-dom';
-import { Box, Typography, makeStyles, Button, Divider } from '@material-ui/core';
+import React, { useState, useContext } from "react";
+import Interviews from "./Interviews";
+import Jobs from "./Jobs";
+import ItvsTemp from "./Interviews";
+import Applications from "./Applications";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useParams,
+  useMatch,
+} from "react-router-dom";
+import {
+  Box,
+  Typography,
+  makeStyles,
+  Button,
+  Divider,
+} from "@material-ui/core";
 
-import { LoginContext } from '../../context/ContextProvider';
+import { LoginContext } from "../../context/ContextProvider";
 
-const useStyle = makeStyles(theme => ({
-    navLinks: {
-        width: '100%',
-        backgroundColor: 'transparent',
-        listStyle: 'none',
-        justifyContent: 'space-around',
-        display: 'flex',
-        fontWeight: 600,
-        '& li': {
-            fontSize: '2em',
-            cursor: 'pointer',
-            // '& ::after': {
-            //     content: "",
-            //     display: 'block',
-            //     width: 0,
-            //     height: '2px',
-            //     background: '#000000',
-            //     transition: 'width 0.3s'
-            // }
-            '& NavLink': {
-                textDecoration: 'none'
-            }
-        }
+const useStyle = makeStyles((theme) => ({
+  navLinks: {
+    width: "100%",
+    backgroundColor: "transparent",
+    listStyle: "none",
+    justifyContent: "space-around",
+    display: "flex",
+    fontWeight: 600,
+    "& li": {
+      fontSize: "2em",
+      cursor: "pointer",
+      // '& ::after': {
+      //     content: "",
+      //     display: 'block',
+      //     width: 0,
+      //     height: '2px',
+      //     background: '#000000',
+      //     transition: 'width 0.3s'
+      // }
+      "& NavLink": {
+        textDecoration: "none",
+      },
     },
-    // navLinks: {
-    //     '& li': {
-    //         fontSize: '0.9em',
-    //         cursor: 'pointer'
-    //     }
-    // },
-    activlink: {
-        // '& li': {
-            content: "",
-            display: 'block',
-            width: 0,
-            height: '2px',
-            background: '#000000',
-            transition: 'width 0.3s',
-            width: '100%'
-        // }
-    },
-    // nav: {
-    //     width: '100%',
-    //     position: 'fixed',
-    //     top: 0,
-    //     height: '70px',
-    //     alignItems: 'center',
-    //     fontSize: '1.1em',
-    //     fontWeight: 600,
-    //     display: 'flex',
-    //     backgroundColor: 'transparent',
-    //     transition: 'all 0.5s linear',
-    //     zIndex: 1000,
-    //     '& active': {
-    //         background: 'white',
-    //         boxShadow: '0 5px 5px -2px rgba(0, 0, 0, 0.4)'
-    //     }
+  },
+  // navLinks: {
+  //     '& li': {
+  //         fontSize: '0.9em',
+  //         cursor: 'pointer'
+  //     }
+  // },
+  activlink: {
+    // '& li': {
+    content: "",
+    display: "block",
+    width: 0,
+    height: "2px",
+    background: "#000000",
+    transition: "width 0.3s",
+    width: "100%",
     // }
-    }));
+  },
+  // nav: {
+  //     width: '100%',
+  //     position: 'fixed',
+  //     top: 0,
+  //     height: '70px',
+  //     alignItems: 'center',
+  //     fontSize: '1.1em',
+  //     fontWeight: 600,
+  //     display: 'flex',
+  //     backgroundColor: 'transparent',
+  //     transition: 'all 0.5s linear',
+  //     zIndex: 1000,
+  //     '& active': {
+  //         background: 'white',
+  //         boxShadow: '0 5px 5px -2px rgba(0, 0, 0, 0.4)'
+  //     }
+  // }
+}));
 
 const Myinfo = () => {
+  const [activeTab, setActiveTab] = useState("Interviews");
 
-    const [activeTab, setActiveTab] = useState('Interviews');
+  const { account, setAccount } = useContext(LoginContext);
 
-    const { account, setAccount } = useContext(LoginContext);
+  const handleClick = (tab) => {
+    setActiveTab(tab);
+  };
 
-    const handleClick = (tab) => {
-        setActiveTab(tab);
-    };
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "Interviews":
+        return <Interviews />;
+      case "Applications":
+        return <Applications />;
+      case "Jobs":
+        return <Jobs />;
+      default:
+        return <Interviews />;
+    }
+  };
 
-    const renderTabContent = () => {
-        switch (activeTab) {
-          case 'Interviews':
-            return <Interviews />;
-          case 'Applications':
-            return <Applications />;
-          case 'Jobs':
-            return <Jobs />;
-          default:
-            return <Interviews />;
-        }
-      };
-
-    return (
-        <div className="App">
-        <nav>
-          <ul>
-            <li onClick={() => handleClick('Interviews')} className={activeTab === 'Interviews' ? 'active' : ''}>Interviews</li>
-            <li onClick={() => handleClick('Applications')} className={activeTab === 'Applications' ? 'active' : ''}>Applications</li>
-            <li onClick={() => handleClick('Jobs')} className={activeTab === 'Jobs' ? 'active' : ''}>Jobs</li>
-          </ul>
-        </nav>
-        <div className="content">{renderTabContent()}</div>
-      </div>
-    )
-}
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li
+            onClick={() => handleClick("Interviews")}
+            className={activeTab === "Interviews" ? "active" : ""}
+          >
+            Interviews
+          </li>
+          <li
+            onClick={() => handleClick("Applications")}
+            className={activeTab === "Applications" ? "active" : ""}
+          >
+            Applications
+          </li>
+          <li
+            onClick={() => handleClick("Jobs")}
+            className={activeTab === "Jobs" ? "active" : ""}
+          >
+            Jobs
+          </li>
+        </ul>
+      </nav>
+      <div className="content">{renderTabContent()}</div>
+    </div>
+  );
+};
 
 export default Myinfo;
